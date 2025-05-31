b35164d64de830f011658a9f77d1adf6bb803830
humaysafarova <166644215+humaysafarova@users.noreply.github.com>
2025-05-31 12:51:07 +0000

    third commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
                 let vall = $("#inputme").val()
                     , newH3 = $('<h3> </h3>')
                     , newDiv = $('<div class="line"> </div>')
-                    , bigDiv = $('<div ${myclass} > </div>')
+                    , bigDiv = $(`<div ${myclass} > </div>`)
                 $(newH3).append(vall)
                 $(newDiv).append(newH3)
                 $(bigDiv).append(newDiv)
@@ -19,11 +19,11 @@ $(document).ready(function () {
             }
         }
 
-        // $("#messageyou").append(myel())
+        $("#messageyou").append(myel())
         $("#messageme").append(myel('class="end"'))
         $("#inputme").val("")
-        // document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
-        // document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
+        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
+        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
 
     });
 
