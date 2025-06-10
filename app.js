1d7b9355675b55d8c54cbcfae516e3115ae4e3d3
humaysafarova <166644215+humaysafarova@users.noreply.github.com>
2025-06-10 13:12:27 +0000

    committed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             if (!$.trim($("#inputme").val()) == '') {
                 let vall = $("#inputme").val()
                     , newH3 = $('<h3> </h3>')
-                    , newDiv = $('<div class="line"> </div>')
+                    , newDiv = $('<div class="line" > </div>')
                     , bigDiv = $(`<div ${myclass} > </div>`)
                 $(newH3).append(vall)
                 $(newDiv).append(newH3)
@@ -22,9 +22,35 @@ $(document).ready(function () {
         $("#messageyou").append(myel())
         $("#messageme").append(myel('class="end"'))
         $("#inputme").val("")
-        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
-        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
+        $("#messageyou").scrollTop($("#messageyou")[0].scrollHeight);
+        $("#messageme").scrollTop($("#messageme")[0].scrollHeight);
 
     });
+
+
+    $("#you").click(function (e) {
+        e.preventDefault();
+        function myel(myclass = "") {
+            if (!$.trim($("#inputyou").val()) == '') {
+                let vall = $("#inputyou").val()
+                    , newH3 = $('<h3> </h3>')
+                    , newDiv = $('<div class="line" > </div>')
+                    , bigDiv = $(`<div ${myclass} > </div>`);
+                $(newH3).append(vall);
+                $(newDiv).append(newH3);
+                $(bigDiv).append(newDiv);
+                return bigDiv;
+            }
+            else {
+                return;
+            }
+        }
+        $("#messageyou").append(myel('class="end"'));
+        $("#messageme").append(myel());
+        $("#inputyou").val("");
+        $("#messageyou").scrollTop($("#messageyou")[0].scrollHeight); 
+        $("#messageme").scrollTop($("#messageme")[0].scrollHeight);
+    });
+
 
 });
